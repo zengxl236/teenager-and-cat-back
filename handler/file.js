fb6c1332bc43092a514a35312824e0cd231461ba
zengxl236 <zengxl@soundtao.com>
2022-09-27 18:06:19 +0800

    upload

--- handler/file.js
+++ handler/file.js
@@ -1,6 +1,7 @@
 const db = require("../db");
 const fs = require("fs");
 const path = require("path");
+const multiparty = require("multiparty");
 
 module.exports = {
   exists_handler: (req, res) => {
@@ -25,18 +26,49 @@ module.exports = {
       if (!fs.existsSync(dirPath)) {
         res.send({
           code: 200,
-          message: "该文件未上传上传",
+          message: "该文件未上传",
           data: [],
         });
+        fs.mkdirSync(dirPath);
         return;
       }
 
       const files = fs.readdirSync(dirPath);
+      res.send({
+        code: 200,
+        message: "该文件部分上传",
+        data: files.map((file) => file.name.split(".")[0]),
+      });
+    });
+  },
+
+  upload_handler: (req, res) => {
+    const form = new multiparty.Form();
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        throw new Error("获取数据失败");
+      }
+
+      const hash = fields.hash[0];
+      const size = fields.size[0];
+      const name = fields.name[0];
+      const chunks = fields.chunks[0];
+      const chunkIdx = fields.chunkIdx[0];
+      const currentHash = fields.currentHash[0];
+      const file = files.file[0];
+
+      const dirPath = path.join(
+        __dirname,
+        `../file/${hash}_${size}`,
+        `${chunkIdx}.temp`
+      );
+
+      fs.writeFileSync(dirPath, file);
 
       res.send({
         code: 200,
-        message: "该文件未上传上传",
-        data: files.map((file) => file.name.split(".")[0]),
+        message: "分片上传成功",
+        data: null,
       });
     });
   },
--- router/file.js
+++ router/file.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const { exists_schema } = require("../schema/file");
-const { exists_handler } = require("../handler/file");
+const { exists_handler, upload_handler } = require("../handler/file");
 
-// 登录接口
+// 查看文件是否上传 上传片段接口
 router.get("/exists", exists_schema, exists_handler);
+// 分片上传接口
+router.post("/upload", upload_handler);
 
 module.exports = router;
